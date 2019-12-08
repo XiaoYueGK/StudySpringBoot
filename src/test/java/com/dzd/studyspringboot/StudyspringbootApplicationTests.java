48bed2a760f884408e6f35de99d13605bad9da90
demo_key_email_qq <2683824605@qq.com>
2019-12-08 20:10:36 +0800

    起步

---
+++ src/test/java/com/dzd/studyspringboot/StudyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzd.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
